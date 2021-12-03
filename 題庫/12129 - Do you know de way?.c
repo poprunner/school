ab573a5e2145cd21d0560ec42f4fafe788f4db10
L_SH <93489493+poprunner@users.noreply.github.com>
2021-12-03 19:49:37 +0800

    Create 12129 - Do you know de way?.c

---
+++ 題庫/12129 - Do you know de way?.c
@@ -0,0 +1,20 @@
+/*#include<stdio.h>
+int main(void){
+	int l,r,x,y;
+	scanf("%d%d",&l,&r);
+	x=l;
+	y=x*2;
+	printf("%d %d\n",x,y);
+	return 0;
+}*/
+
+
+
+#include <stdio.h>
+int main()
+{
+	int l;
+	scanf("%d%*d",&l);
+	printf("%d %d\n",l,l*2);
+	return 0;
+}
