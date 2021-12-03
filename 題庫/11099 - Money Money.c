7a2556cadb4762910218d80d6e641a2571ada147
L_SH <93489493+poprunner@users.noreply.github.com>
2021-12-03 19:54:13 +0800

    Create 11099 - Money Money.c

---
+++ 題庫/11099 - Money Money.c
@@ -0,0 +1,21 @@
+/*#include <stdio.h>
+int main(){
+	int a,b,c,sum;
+	scanf("%d%d%d",&a,&b,&c);
+	sum=(a+b+c)/2;
+	printf("%d %d %d\n",sum-b,sum-c,sum-a);
+	return 0;
+}*/
+
+
+
+
+#include <stdio.h>
+int main()
+{
+	int jajo, jojj, jajj, tt;
+	scanf("%d%d%d",&jajo,&jojj,&jajj);
+	tt = (jajo+jojj+jajj)/2;
+	printf("%d %d %d\n",tt-jojj,tt-jajj,tt-jajo);
+	return 0;
+}
