87b465586f508e7edbdf16126ce8d5f0eac87a38
L_SH <93489493+poprunner@users.noreply.github.com>
2021-12-03 19:57:26 +0800

    Create 10716 - Arithmetic Sequence.c

---
+++ 題庫/10716 - Arithmetic Sequence.c
@@ -0,0 +1,19 @@
+/*#include <stdio.h>
+int main(){
+	int a,n,d;
+	scanf("%d%d%d",&a,&n,&d);
+	printf("%d %d",a+(n-1)*d,(2*a+(n-1)*d)*n/2);
+	return 0;
+}*/
+
+
+
+
+#include <stdio.h>
+int main()
+{
+	int a, n, r;
+	scanf("%d%d%d",&a,&n,&r);
+	printf("%d %d",a+(n-1)*r,(2*a+(n-1)*r)*n/2);
+	return 0;
+}
