a49ef7d25ab2a1e39835d17b7df121802f018d42
L_SH <93489493+poprunner@users.noreply.github.com>
2021-12-03 19:56:04 +0800

    Create 12675 - Get the number.c

---
+++ 題庫/12675 - Get the number.c
@@ -0,0 +1,19 @@
+/*#include <stdio.h>
+int main(){
+	int a;
+	scanf("%d",&a);
+	printf("%d %d",a*9,a*8);
+	return 0;
+}*/
+
+
+
+
+#include <stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	printf("%d %d\n",a*9,a*8);
+	return 0;
+}
